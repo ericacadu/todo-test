e26c554677767d2bb8586293b431fb34780f1943
ashramwen <awen@aviatrix.com>
2022-03-04 09:40:32 +0800

    refactor: 💡 Update route

--- src/pages/Cola/index.js
+++ src/pages/Cola/index.js
@@ -1,2 +1,16 @@
-import Cola from './Cola';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import TodoList from './TodoList';
+
+function Cola() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<TodoList />} />
+        <Route path="todo" element={<TodoList />} />
+      </Route>
+    </Routes>
+  );
+}
+
 export default Cola;
